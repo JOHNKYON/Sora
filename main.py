20fe73d5135e91c1ff235497bb6d0c6f773a329e
JOHNKYON <zhao_fengbo@outlook.com>
2018-05-08 00:38:21 +0900

    Rename "Models" to "models_demo"

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+"""This is the entrance of the modules, most time used for development"""
+
 def main():
     print("Hello World! Here we're starting to see what's happening inside your model! ")
+
+    print("\033[0;33mStart decision tree test\033[0m")
 
 
 if __name__ == "__main__":
