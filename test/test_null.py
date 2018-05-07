9ce12aac2e0ed6b78654056063e46266cf24b35d
JOHNKYON <zhao_fengbo@outlook.com>
2018-05-07 13:52:01 +0900

    Add .travis.yml and test-null, in order to use Travis-CI

---
+++ test/test_null.py
@@ -0,0 +1,11 @@
+"""This is the test case that do nothing, it's just used to pass travis-CI when no tests are
+available"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def testnull():
+    """Test case that do nothing"""
+    pass
