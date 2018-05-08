04d803124e07bc3152f9a62f38c476e41664b3b5
JOHNKYON <zhao_fengbo@outlook.com>
2018-05-08 18:14:42 +0900

    Finish writing a simple demo of getting a trained decision tree regressor

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """This is the entrance of the modules, most time used for development"""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
 def main():
     print("Hello World! Here we're starting to see what's happening inside your model! ")
-
-    print("\033[0;33mStart decision tree test\033[0m")
 
 
 if __name__ == "__main__":
